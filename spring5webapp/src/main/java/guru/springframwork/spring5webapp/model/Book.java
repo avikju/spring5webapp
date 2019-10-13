a9f963c1c428d058315fb43de9e4625eb32d0778
avikju <avikju@gmail.com>
2019-10-13 12:09:36 +0530

    first commit

---
+++ spring5webapp/src/main/java/guru/springframwork/spring5webapp/model/Book.java
@@ -0,0 +1,121 @@
+package guru.springframwork.spring5webapp.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	private String isbn;
+	private String title;
+	private String publisher;
+	
+	@ManyToMany
+	@JoinTable(name="author_book",joinColumns=@JoinColumn(name="book_id"),inverseJoinColumns=@JoinColumn(name="author_id"))
+	private Set<Author> authors=new HashSet<>();
+	
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Book(String isbn, String title, String publisher) {
+		super();
+		this.isbn = isbn;
+		this.title = title;
+		this.publisher = publisher;
+	}
+
+	
+
+	public Book(String isbn, String title, String publisher, Set<Author> authors) {
+		super();
+		this.isbn = isbn;
+		this.title = title;
+		this.publisher = publisher;
+		this.authors = authors;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", isbn=" + isbn + ", title=" + title + ", publisher=" + publisher + ", authors="
+				+ authors + "]";
+	}
+
+}
---
+++ spring5webapp/src/main/java/guru/springframwork/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package guru.springframwork.spring5webapp.repositories;
+
+/**
+ * @author Avik
+ *
+ */
+public class AuthorRepository {
+
+}
